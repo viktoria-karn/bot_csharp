62ad14de37016ca1652f1ef1a7b094c9857aba00
viktoria-karn <66881490+viktoria-karn@users.noreply.github.com>
2021-05-01 17:45:04 +0700

    Реализация запроса контакта (номера телефона)

--- my_bot/Bot.cs
+++ my_bot/Bot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace CSharpBot
 {
@@ -19,8 +20,51 @@ namespace CSharpBot
         /// <param name="e"></param>
         private void MessageProcessor(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            client.SendTextMessageAsync(e.Message.Chat.Id, $"Привет!");
-            Console.WriteLine(e.Message.Text);
+            switch (e.Message.Type)
+            {
+                
+                case Telegram.Bot.Types.Enums.MessageType.Contact:
+                    string phone = e.Message.Contact.PhoneNumber;
+                    client.SendTextMessageAsync(e.Message.Chat.Id, $"Твой телефон {phone}");
+                    Console.WriteLine(phone);
+                    break;
+                case Telegram.Bot.Types.Enums.MessageType.Text:
+                    if (e.Message.Text.Substring(0, 1) == "/")
+                    {
+                        CommandProcessor(e.Message);
+                    }
+                    else{
+                        client.SendTextMessageAsync(e.Message.Chat.Id, $"Ты мне сказал {e.Message.Text} ,но я этого пока не понимаю");
+                        Console.WriteLine(e.Message.Text);
+                    }
+                    break;
+            }
+          
+        }
+        /// <summary>
+        /// обработка команд
+        /// </summary>
+        /// <param name=""></param>
+        private void CommandProcessor(Telegram.Bot.Types.Message message)
+        {
+            string command = message.Text.Substring(1).ToLower();
+
+            switch (command)
+            {
+                case "start":
+                    var button = new KeyboardButton("Поделись телефоном");
+                    var array = new KeyboardButton[] {button};
+                    button.RequestContact = true; //кнопка запрашивает контакт
+                    var reply = new ReplyKeyboardMarkup(array,true,true);
+                    client.SendTextMessageAsync(message.Chat.Id, $"Привет,{message.Chat.FirstName} ,введи свой номер телефона",replyMarkup:reply);
+                    break;
+                default:
+                    client.SendTextMessageAsync(message.Chat.Id, $"Ты мне сказал {message.Text} ,но я этого пока не понимаю");
+                    break;
+
+
+            }
+
         }
         public void Run()
         {
